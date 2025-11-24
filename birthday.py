3ef153bdfb2159c072c12563d8bdf3500ab00261
Aarohi098 <aarohimahajan007@gmail.com>
2025-11-24 21:08:27 +0000

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,7 @@
+hannah = 31,"January"
+saanvi = 1,"September"
+simran = 29,"November"
+keshini = 16,"September"
+maariyah = 16,"October"     
+
+print("Hannah-", hannah ,"\n","Saanvi-", saanvi,"\n" ,"Simran-",simran,"\n" , "Keshini-",keshini,"\n" ,"Maariyah-", maariyah)  
