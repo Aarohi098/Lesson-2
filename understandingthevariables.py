33b1cc6d2627283d3f9a4efabac262d9e00389d7
Aarohi098 <aarohimahajan007@gmail.com>
2025-11-24 17:44:32 +0000

    Add files via upload

---
+++ understandingthevariables.py
@@ -0,0 +1,11 @@
+y = 6
+x = "Hello"
+print(y)
+print(x)
+
+codingal = "codingal"
+print("codingal")
+
+userinput = input("Enter your Name")
+print("\nHello", userinput, "\nwelcome to codingal")
+print("hello")
---
+++ welcom.py
@@ -0,0 +1,16 @@
+#print a sentence 
+print("Welcome to the world of programming")
+
+#print an interger
+print(4)
+
+#make another line
+print("Hello \n")
+
+#2 multiple arguements for printing
+print("Hello", 5)
+
+# use end arguement of the printing statement
+print("Welcome to ", end=" ") 
+print("programming")
+print("hello")
